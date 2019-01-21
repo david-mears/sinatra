c7a805506c97613c0c7e4839c71601ad7244bb75
david-mears <kieukmail@gmail.com>
2019-01-21 16:31:43 +0000

    Using forms

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 
 
-get '/cat' do
-  @variable = ['1', '2', '3'].sample 
+get '/random-cat' do
+  @variable = ['1', '2', '3'].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @color = params[:color]
   erb(:index)
 end
